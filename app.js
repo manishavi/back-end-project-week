f5f420d6c18972dbb31b976b3437f7eea2cf7b87
manishavi <manishalal972@gmail.com>
2018-04-04 11:29:43 -0500

    adding mongo server

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const { server } = require("./server.js");
 const config = require("./config.json");
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/notes', () => {
+    console.log('connected to mongodb')
+});
+
 
 const PORT = process.env.PORT || 3000;
 
--- server.js
+++ server.js
@@ -10,13 +10,23 @@ const BCRYPT_COST = 11;
 server.use(express.json());
 server.use(
     session({
-        secret: config.SECRET,
+        secret: 'lzEIGyov8aXTzq80V8YMY8MTffp4imV93FngTXwD4bzmnepFNN',
         resave: true,
         saveUninitialized: false
     })
 );
 
+const sendUserError = (err, res) => {
+    res.status(STATUS_USER_ERROR);
+    if (err && err.message) {
+        res.json({ message: err.message, stack: err.stack });
+    } else {
+        res.json({ error: err });
+    }
+};
+
 server.get('/', (req, res) => {
     console.log('Get route is working');
+    res.json({ success: true });
 });
 module.exports = { server };
